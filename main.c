b65ccef5aad2c86197edbd1ce735c4f26f7fc662
yerimlee <144992260+limye2@users.noreply.github.com>
2025-10-30 16:15:25 +0900

    실습4

--- main.c
+++ main.c
@@ -6,25 +6,34 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
+
+void square_array(int a[], int size);
+void print_array(int a[], int size);
 
 int main(int argc, const char * argv[]) {
-    int i;
-    int a[SIZE] = {1,2,3,4,5};
-    int b[SIZE] = {1,2,3,4,6};
-    int flag_same = 1;
-
-    for(i=0; i<SIZE; i++)
-    {
-        if(a[i] != b[i])
-        {
-            printf("%i-th element is diff.\n", i);
-            flag_same = 0;
-        }
-    }
+    int list[SIZE] = {1,2,3,4};
     
-    printf("Are a and b are the same? %i\n", flag_same);
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
     
     return 0;
+}
+
+void square_array(int a[], int size)
+{
+    int i;
+    
+    for(i=0; i<size; i++)
+        a[i] = a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+    int i;
+    
+    for(i=0; i<size; i++)
+        printf("%3d", a[i]);
+    printf("\n");
 }
